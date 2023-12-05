d4a2c7fcf3d51344a552f110004529b2d7129f46
carsonchan12345 <44266907+carsonchan12345@users.noreply.github.com>
2023-12-05 14:58:42 +0800

    Add files via upload

---
+++ pingsweep.ps1
@@ -0,0 +1,14 @@
+﻿$InputFile = 'C:\Temp\MachineList.txt'
+$machines = Get-Content $InputFile
+
+foreach ($machine in $machines) {
+    $pingResult = Test-Connection -ComputerName $machine -Count 1 -ErrorAction SilentlyContinue
+
+    if ($pingResult) {
+        $ipAddress = $pingResult.IPV4Address.IPAddressToString
+        Write-Host "$machine, up ($ipAddress)" -ForegroundColor Green
+    }
+    else {
+        Write-Host "$machine, down" -ForegroundColor Red
+    }
+}
